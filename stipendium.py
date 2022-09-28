f009c4963824ef92a84449b884fd66138d8e7453
Gregory Barnes <frbarnes@GregoryacStudio.lan>
2022-09-27 22:15:21 -0400

    clean up comments

--- stipendium.py
+++ stipendium.py
@@ -19,6 +19,9 @@ app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///'+os.path.join(basedir, 'book.db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SECRET_KEY'] = 'txWufrjMigCiVQJF2TBmiA'
+
+# TODO: make a startup script for initializing the database
+
 db = SQLAlchemy(app)
 
 
@@ -38,9 +41,7 @@ class Stipend(db.Model):
     requester     = db.Column(db.String(25), unique=False, nullable=False)
     origin        = db.Column(db.String(6), unique=False, nullable=False)
     accepted      = db.Column(db.DateTime, default=datetime.now)
-    # request_date  = db.Column(db.Boolean, default=False, nullable=False)
     req_date      = db.Column(db.DateTime, nullable=True)
-    # finish        = db.Column(db.DateTime, nullable=True)
     amount        = db.Column(db.Integer, nullable=False)
     masses        = db.Column(db.Integer, default=1, nullable=False)
     celebrant     = db.Column(db.String(25), unique=False, nullable=True)
