16a0b89a41815fb7224baed09d95146d4d3d5ef6
corei8 <corei8.github@gmail.com>
2022-10-26 16:44:05 -0400

    merge initial configuration

--- stipendium/__init__.py
+++ stipendium/__init__.py
@@ -1,4 +1,4 @@
-from settings import DevelopmentConfig
+# from settings import DevelopmentConfig
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from os import path
@@ -6,12 +6,12 @@ from os import path
 basedir = path.abspath(path.dirname(__file__))
 
 app = Flask(__name__)
-# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///'+path.join(basedir, 'book.db')
-# app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-# app.config['SECRET_KEY'] = 'txWufrjMigCiVQJF2TBmiA'
-# app.config['DEBUG'] = True
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///'+path.join(basedir, 'book.db')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SECRET_KEY'] = 'txWufrjMigCiVQJF2TBmiA'
+app.config['DEBUG'] = True
 
-app.config.from_object('settings.DevelopmentConfig')
+# app.config.from_object('settings.DevelopmentConfig')
 
 db = SQLAlchemy(app)
 
