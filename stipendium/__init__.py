59bd187fb537af7b78d107c13b78b6ea4fa86045
corei8 <corei8.github@gmail.com>
2022-12-15 17:40:04 -0500

    add environment variables

--- stipendium/__init__.py
+++ stipendium/__init__.py
@@ -6,6 +6,10 @@ from flask_login import (
         )
 from os import path, mkdir
 
+# from dotenv import load_dotenv
+
+# load_dotenv()
+
 basedir = path.abspath(path.dirname(__file__))
 ROOT = path.dirname(path.realpath(__file__))
 
@@ -16,13 +20,12 @@ except FileExistsError:
     pass
 
 app = Flask(__name__)
-# TODO: make .env file
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////'+path.join('databases', 'book.db')
-# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///'+path.join('databases', 'book.db')
-# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///'+path.join(ROOT, '/databases', 'book.db')
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config['SECRET_KEY'] = 'txWufrjMigCiVQJF2TBmiA' # make this more secret soon
-app.config['DEBUG'] = True
+
+app.config.from_prefixed_env()
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////'+path.join('databases', 'book.db')
+# app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+# app.config['SECRET_KEY'] = 'txWufrjMigCiVQJF2TBmiA' # make this more secret soon
+# app.config['DEBUG'] = True
 
 # app.config.from_object('settings.DevelopmentConfig')
 
